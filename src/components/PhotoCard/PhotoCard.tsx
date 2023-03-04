cf306695195ae8cc0e1c2aec27ae55e7d5e6e722
carnold84 <carnold84@users.noreply.github.com>
2023-03-04 17:00:15 +1300

    Updated PhotoCard

--- src/components/PhotoCard/PhotoCard.tsx
+++ src/components/PhotoCard/PhotoCard.tsx
@@ -1,5 +1,9 @@
 import classnames from "classnames";
-import { AnimatePresence, motion } from "framer-motion";
+import {
+  AnimatePresence,
+  ControlsAnimationDefinition,
+  motion,
+} from "framer-motion";
 import { useLayoutEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { Link } from "react-router-dom";
@@ -22,35 +26,32 @@ const MotionLink = motion(Link);
 
 const PhotoCard = ({ imageUrl, isOpen = false, to }: Props) => {
   const elRef = useRef<HTMLDivElement | null>(null);
-  const [modalStyles, setModalStyles] = useState<ModalStyles>();
-
-  const observer = useRef(
-    new ResizeObserver((entries) => {
-      const { height, x, y, width } = entries[0].target.getBoundingClientRect();
-
-      setModalStyles({
-        height,
-        opacity: 1,
-        x,
-        y,
-        width,
-      });
-    })
+  const [modalStyles, setModalStyles] = useState<ModalStyles | null>(null);
+  const [status, setStatus] = useState<"closed" | "open">(
+    isOpen ? "open" : "closed"
   );
 
   useLayoutEffect(() => {
-    setTimeout(() => {
-      if (elRef.current) {
-        observer.current.observe(elRef.current);
+    if (elRef.current) {
+      if (isOpen) {
+        const { height, x, y, width } = elRef.current.getBoundingClientRect();
+        setStatus("open");
+        setModalStyles({
+          height,
+          opacity: 1,
+          x,
+          y,
+          width,
+        });
+      } else {
+        setModalStyles(null);
       }
-    }, 100);
+    }
+  }, [elRef.current, isOpen]);
 
-    return () => {
-      if (elRef.current) {
-        observer.current.unobserve(elRef.current);
-      }
-    };
-  }, [elRef, observer]);
+  const onAnimationComplete = (evt: ControlsAnimationDefinition) => {
+    setStatus("closed");
+  };
 
   return (
     <div
@@ -61,37 +62,65 @@ const PhotoCard = ({ imageUrl, isOpen = false, to }: Props) => {
     >
       <img
         className={classnames("h-full w-full object-cover", {
-          invisible: isOpen,
+          invisible: status !== "closed",
         })}
         src={imageUrl}
       />
       <Link className="absolute top-0 left-0 h-full w-full" to={to} />
       {createPortal(
         <AnimatePresence>
-          {isOpen && (
+          {modalStyles && (
             <motion.div
               animate={{
                 height: "100%",
-                position: "fixed",
+                transition: { delay: 0.05, duration: 0.3, ease: "easeInOut" },
                 x: 0,
                 y: 0,
                 width: "100%",
               }}
-              className="absolute top-0 left-0 h-full w-full"
-              exit={modalStyles}
+              className="fixed top-0 left-0 h-full w-full overflow-hidden"
+              exit={{
+                ...modalStyles,
+                transition: { delay: 0.1, ease: "easeInOut" },
+              }}
               initial={modalStyles}
+              onAnimationComplete={onAnimationComplete}
             >
               <div className="absolute left-0 top-0 h-full w-full">
                 <MotionLink
                   animate={{
-                    opacity: 1,
+                    transform: "translateY(0%)",
+                    transition: {
+                      delay: 0.3,
+                      duration: 0.1,
+                      ease: "easeInOut",
+                    },
                   }}
-                  className="absolute right-0 top-0 bg-white p-4"
-                  exit={{ opacity: 0 }}
-                  initial={{ opacity: 0 }}
+                  className="absolute left-1/2 top-0 -translate-x-1/2 bg-white p-3"
+                  exit={{
+                    transform: "translateY(-100%)",
+                    transition: { duration: 0.1 },
+                  }}
+                  initial={{ transform: "translateY(-100%)" }}
                   to="/"
                 >
-                  Close
+                  <svg
+                    width="24px"
+                    height="24px"
+                    strokeWidth="1.5"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    color="#000000"
+                  >
+                    <path
+                      d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
+                      stroke="#000000"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </svg>
                 </MotionLink>
                 <img className="h-full w-full object-cover" src={imageUrl} />
               </div>
